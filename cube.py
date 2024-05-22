c7274e2517a434c2e33b61db86d8ad59c7f38126
David Veller (Partner) <QNM367@motorolasolutions.com>
2024-05-22 10:32:32 -0400

    we movin the cube kind of it still looks weird and the camera perspective is at 0,0 i THINK

--- cube.py
+++ cube.py
@@ -10,7 +10,6 @@ def main():
     WIDTH = 1280
     HEIGHT = 800
 
-    x_start, y_start = 0, 0
     x_seperator = 10
     y_seperator = 20
 
@@ -29,15 +28,83 @@ def main():
     display_surface = pygame.display.set_mode((WIDTH, HEIGHT))
     #display_surface = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
 
-    pygame.display.set_caption("Square")
+    pygame.display.set_caption("Cube")
     font = pygame.font.SysFont("Arial", 18, bold=True)
 
+    #puts text on screen at position
     def text_display(letter, x_start, y_start):
         text = font.render(str(letter), True, white)
         display_surface.blit(text, (x_start, y_start))
 
-    x_start, y_start = WIDTH / 2, HEIGHT / 2
+    #coords of cube in hypothetical 3d plane
+    # point c is at starting coords
+    #side length is variable "side"
+    #
+    #      a-----b
+    #      |  e----f  
+    #      |  |    |
+    #      c--|--d |
+    #         g----h
+
+    #distance to camera
+    distance_to_camera = 1000
+
     side = 100
+    x_start, y_start, z_start = WIDTH / 2, HEIGHT / 2, 100
+
+    c = (x_start, y_start, z_start)
+    d = (x_start + side, y_start, z_start)
+    a = (x_start, y_start + side, z_start)
+    b = (x_start + side, y_start + side, z_start)
+    e = (x_start, y_start + side, z_start + side)
+    f = (x_start + side, y_start + side, z_start + side)
+    g = (x_start, y_start, z_start + side)
+    h = (x_start + side, y_start, z_start + side)
+
+    #convert to 2d coords version1
+    def perspective_projection(old_coords, camera_distance):
+        x = old_coords[0]
+        y = old_coords[1]
+        z = old_coords[2]
+
+        new_x = (camera_distance/(camera_distance + z)) * x
+        new_y = (camera_distance/(camera_distance + z)) * y
+
+        return (new_x, new_y)
+    
+    #version2
+    #f = focal lengths, c = coords of principal point
+    #basically c means coords of plane?
+    #not sure what f means
+    def to_3d(old_coords, f_x, f_y, c_x, c_y):
+        x = old_coords[0]
+        y = old_coords[1]
+        z = old_coords[2]
+
+        new_x = (x / z) * f_x + c_x
+        new_y = (y / z) * f_y + c_y
+
+        return (new_x, new_y)
+    
+    new_a = perspective_projection(a, distance_to_camera)
+    new_b = perspective_projection(b, distance_to_camera)
+    new_c = perspective_projection(c, distance_to_camera)
+    new_d = perspective_projection(d, distance_to_camera)
+    new_e = perspective_projection(e, distance_to_camera)
+    new_f = perspective_projection(f, distance_to_camera)
+    new_g = perspective_projection(g, distance_to_camera)
+    new_h = perspective_projection(h, distance_to_camera)
+
+    f_x, f_y, c_x, c_y = 10,10,10,10
+    newnew_a = to_3d(a, f_x, f_y, c_x, c_y)
+    newnew_b = to_3d(b, f_x, f_y, c_x, c_y)
+    newnew_c = to_3d(c, f_x, f_y, c_x, c_y)
+    newnew_d = to_3d(d, f_x, f_y, c_x, c_y)
+    newnew_e = to_3d(e, f_x, f_y, c_x, c_y)
+    newnew_f = to_3d(f, f_x, f_y, c_x, c_y)
+    newnew_g = to_3d(g, f_x, f_y, c_x, c_y)
+    newnew_h = to_3d(h, f_x, f_y, c_x, c_y)
+
 
     run = True
 
@@ -47,19 +114,84 @@ def main():
         #displays x and y coords
         #text_display("x:" + str(x) + " y:" + str(y), 0, 0)
 
+        #text_display("Distance to camera: " + str(distance_to_camera), 0, 0)
+
         # keys = pygame.key.get_pressed()
         # if keys[pygame.K_UP]:
-        #     y -= 1
+        #     distance_to_camera -= 1
+        #     if distance_to_camera <= 0:
+        #         distance_to_camera = 1
         # if keys[pygame.K_DOWN]:
-        #     y += 1
+        #     distance_to_camera += 1
         # if keys[pygame.K_LEFT]:
         #     x -= 1
         # if keys[pygame.K_RIGHT]:
         #     x += 1
 
-        #cube
-        pygame.draw.line(screen, white, [x_start, y_start], [x_start + side, y_start])
-        pygame.draw.line(screen, white, [x_start, y_start], [x_start, y_start + side])
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_UP]:
+            f_y -= 1
+        if keys[pygame.K_DOWN]:
+            f_y += 1
+        if keys[pygame.K_LEFT]:
+            f_x -= 1
+        if keys[pygame.K_RIGHT]:
+            f_x += 1
+        if keys[pygame.K_w]:
+            c_y -= 1
+        if keys[pygame.K_s]:
+            c_y += 1
+        if keys[pygame.K_d]:
+            c_x -= 1
+        if keys[pygame.K_a]:
+            c_x += 1
+
+
+        new_a = perspective_projection(a, distance_to_camera)
+        new_b = perspective_projection(b, distance_to_camera)
+        new_c = perspective_projection(c, distance_to_camera)
+        new_d = perspective_projection(d, distance_to_camera)
+        new_e = perspective_projection(e, distance_to_camera)
+        new_f = perspective_projection(f, distance_to_camera)
+        new_g = perspective_projection(g, distance_to_camera)
+        new_h = perspective_projection(h, distance_to_camera)
+
+        newnew_a = to_3d(a, f_x, f_y, c_x, c_y)
+        newnew_b = to_3d(b, f_x, f_y, c_x, c_y)
+        newnew_c = to_3d(c, f_x, f_y, c_x, c_y)
+        newnew_d = to_3d(d, f_x, f_y, c_x, c_y)
+        newnew_e = to_3d(e, f_x, f_y, c_x, c_y)
+        newnew_f = to_3d(f, f_x, f_y, c_x, c_y)
+        newnew_g = to_3d(g, f_x, f_y, c_x, c_y)
+        newnew_h = to_3d(h, f_x, f_y, c_x, c_y)
+
+        #version1 3d
+        # pygame.draw.line(screen, white, new_a, new_b)
+        # pygame.draw.line(screen, white, new_a, new_c)
+        # pygame.draw.line(screen, white, new_a, new_e)
+        # pygame.draw.line(screen, white, new_b, new_d)
+        # pygame.draw.line(screen, white, new_b, new_f)
+        # pygame.draw.line(screen, white, new_c, new_d)
+        # pygame.draw.line(screen, white, new_c, new_g)
+        # pygame.draw.line(screen, white, new_d, new_h)
+        # pygame.draw.line(screen, white, new_e, new_f)
+        # pygame.draw.line(screen, white, new_e, new_g)
+        # pygame.draw.line(screen, white, new_f, new_h)
+        # pygame.draw.line(screen, white, new_g, new_h)
+
+        #version2 3d
+        pygame.draw.line(screen, white, newnew_a, newnew_b)
+        pygame.draw.line(screen, white, newnew_a, newnew_c)
+        pygame.draw.line(screen, white, newnew_a, newnew_e)
+        pygame.draw.line(screen, white, newnew_b, newnew_d)
+        pygame.draw.line(screen, white, newnew_b, newnew_f)
+        pygame.draw.line(screen, white, newnew_c, newnew_d)
+        pygame.draw.line(screen, white, newnew_c, newnew_g)
+        pygame.draw.line(screen, white, newnew_d, newnew_h)
+        pygame.draw.line(screen, white, newnew_e, newnew_f)
+        pygame.draw.line(screen, white, newnew_e, newnew_g)
+        pygame.draw.line(screen, white, newnew_f, newnew_h)
+        pygame.draw.line(screen, white, newnew_g, newnew_h)
 
         pygame.display.update()
 
@@ -67,26 +199,6 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
-
-# def create_square_array(side):
-#     square_array = [[0 for j in range(side)] for i in range(side + 1)] 
-#     #top side
-#     for i in range(side):
-#         square_array[0][i] = "_"
-#     #left and right sides
-#     for i in range(1, side):
-#         square_array[i][0] = "|"
-#         square_array[i][side - 1] = "|"
-#     #bottom side
-#     for i in range(side):
-#         square_array[side][i] = "_"
-#     return square_array
-
-# def draw(array):
-#     for i in range(len(array)):
-#         for j in range(len(array[i])):
-#             print(array[i][j], end="")
-#         print()
 
 if __name__ == "__main__":
     main()
